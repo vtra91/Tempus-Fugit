40fec2e526ff8e4cfe64c1bdb6f6335531055d5d
vtra9 <melnikov0109@gmail.com>
2026-03-29 22:20:21 +0800

    Украсивил немного, но все еще не получается избавиться от визуального бага с телепортацией текста

---
+++ View/MainBlocks/MainBlocksV.swift
@@ -0,0 +1,18 @@
+//
+//  MainBlocksV.swift
+//  Tempus Fugit
+//
+//  Created by Сергей Мельников on 27.03.2026.
+//
+
+import SwiftUI
+
+struct MainBlocksV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainBlocksV()
+}
